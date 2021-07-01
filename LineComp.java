0c7325972e60b90dc560c589bfeacb978abcd2da
sknithin06 <nithinsk939@gmail.com>
2021-07-01 18:55:51 +0530

    UC2 is added

--- LineComp.java
+++ LineComp.java
@@ -16,5 +16,26 @@ public class LineComp {
 		double p = 2;
 		double x= Math.pow((x2 - x1),p) + Math.pow((y2- y1),p);
 		System.out.println(Math.sqrt(x));
+		
+		System.out.print("Enter first number- ");  
+		double a1=sc.nextInt();  
+		System.out.print("Enter second number- ");  
+		double a2=sc.nextInt();  
+		System.out.print("Enter third number- ");  
+		double b1= sc.nextInt();  
+		System.out.print("enter fourth number-");
+		double b2= sc.nextInt();
+		System.out.print("Enter first number- ");  
+		double c1=sc.nextInt();  
+		System.out.print("Enter second number- ");  
+		double c2=sc.nextInt(); 
+		if ( (a1 / a2 == b1 / b2)
+				&& (b1 / b2 == c1 / c2)
+				&& (a1 / a2 == c1 / c2) ) 
+			System.out.println("Lines are identical");
+		else
+			System.out.println("Lines are not identical");
+		
+		
 	}
 }
